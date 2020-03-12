7daddb1e0324399979f0e273eb8b5c4f69bbf5a1
Walter Merscher <40008264+Wu1z@users.noreply.github.com>
2020-03-12 17:30:17 -0300

    Add files via upload

---
+++ build/precache-manifest.cfe4a8fb55317393e700ccc8319f26a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f340e9e063ba46fabf5947c1ba703e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0c307079f9d5204e47d",
+    "url": "/static/css/main.b38a6e85.chunk.css"
+  },
+  {
+    "revision": "1333cede8f59a9340d9b",
+    "url": "/static/js/2.a31407dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a31407dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c307079f9d5204e47d",
+    "url": "/static/js/main.53d060fa.chunk.js"
+  },
+  {
+    "revision": "4861f834cac1ffca3b5a",
+    "url": "/static/js/runtime-main.df271c61.js"
+  }
+]);
